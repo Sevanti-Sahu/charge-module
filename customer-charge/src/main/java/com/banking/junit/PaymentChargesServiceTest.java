de7a684bdedab94055694d6d69bd1d86156da9ca
sevanti <sevantisahu31@gmail.com>
2019-05-18 19:44:27 -1200

    refined junit for service

--- customer-charge/src/main/java/com/banking/junit/PaymentChargesServiceTest.java
+++ customer-charge/src/main/java/com/banking/junit/PaymentChargesServiceTest.java
@@ -38,11 +38,14 @@ public class PaymentChargesServiceTest {
 	
 	ChargesRequestModel requestdata ;
 	
+	PaymentchargesService paymentChargesService;
+	
 	@Before
 	public void before() throws IOException
 	{	
 	String path = "C:\\Users\\DELL\\git\\paymentCharge\\customer-charge\\src\\main\\resources\\static\\request.json";
 	ReadFileAsString read = new ReadFileAsString();
+	paymentChargesService = new PaymentchargesService(paymentChargesRepository);
 	String str = read.readFileAsString(path);
 	ObjectMapper mapper = new ObjectMapper();
 	requestdata = mapper.readValue(str, ChargesRequestModel.class);	
@@ -62,7 +65,6 @@ public class PaymentChargesServiceTest {
 	@Test
 	public void testServiceForFxCharges() throws IOException, CurrencyNotFoundException
 	{
-		PaymentchargesService paymentChargesService = new PaymentchargesService(paymentChargesRepository);
 		Fxcharges fxcharge = new Fxcharges();
 		fxcharge.setCountry("AT");
 		fxcharge.setLanguage("En");
@@ -70,14 +72,12 @@ public class PaymentChargesServiceTest {
 		when(paymentChargesRepository.getFxCharges(Mockito.anyString())).thenReturn(fxcharge);
 		ResponseEntity<?> response = paymentChargesService.getPaymentCharges(requestdata);
 		String path = "C:\\Users\\DELL\\git\\paymentCharge\\customer-charge\\src\\main\\resources\\static\\fx_response.json";
-		afterTest(path,response);			
-		
+		afterTest(path,response);					
 	}
 
 	@Test
 	public void testServiceForNormalCharges() throws IOException, CurrencyNotFoundException
 	{
-		PaymentchargesService paymentChargesService = new PaymentchargesService(paymentChargesRepository);
 		requestdata.getInstructedamount().setCurrency("EUR");
 		requestdata.setChargebearer("DEBT");
 		Normalcharges normalcharges = new Normalcharges();
@@ -99,7 +99,6 @@ public class PaymentChargesServiceTest {
 	@Test
 	public void testServiceForBothCharges() throws IOException, CurrencyNotFoundException
 	{
-		PaymentchargesService paymentChargesService = new PaymentchargesService(paymentChargesRepository);
 		requestdata.setChargebearer("DEBT");
 		Fxcharges fxcharge = new Fxcharges();
 		fxcharge.setCountry("AT");
@@ -127,7 +126,6 @@ public class PaymentChargesServiceTest {
 	@Test
 	public void testServiceForNoCharges() throws IOException, CurrencyNotFoundException
 	{
-		PaymentchargesService paymentChargesService = new PaymentchargesService(paymentChargesRepository);
 		requestdata.getInstructedamount().setCurrency("EUR");
 		ResponseEntity<?> response = paymentChargesService.getPaymentCharges(requestdata);
 		ChargesResponse chargesResponse = new ChargesResponse();
